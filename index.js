ee76b61abc54a06d0abeb5aac3347904f73b1eb7
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-04 11:43:06 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+
+
+
+/*const cats=["Milo", "Otis", "Garfield"];
+cats.push("cat");
+console.log(cats);
+cats.unshift("cat");
+cats.pop();
+cats.unshift();*/
+
+
+
+
+
+ const cats=  ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+function appendCat(name){
+    return [...cats, name]
+}
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+    
+}
+re
+ 
+
+
+console.log(cats); 
+destructivelyAppendCat("Ralp");
